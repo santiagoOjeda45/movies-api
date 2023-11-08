3f8651fe8f2c37376ab50fe096f6add5292fb238
Santiago Ojeda <134347402+santiagoOjeda45@users.noreply.github.com>
2023-11-07 19:27:23 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const actorRouter = require('./actor.router');
+const directorRouter = require('./director.router');
+const genreRouter = require('./genre.controllers');
+const movieRouter = require('./movie.router');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use('/actors', actorRouter);
+router.use('/directors', directorRouter);
+router.use('/genres', genreRouter);
+router.use('/movies', movieRouter);
 
 
 module.exports = router;
---
+++ src/routes/movie.router.js
@@ -0,0 +1,15 @@
+const { getAll, create, getOne, remove, update } = require('../controllers/movie.controllers');
+const express = require('express');
+
+const movieRouter = express.Router();
+
+movieRouter.route('/')
+    .get(getAll)
+    .post(create);
+
+movieRouter.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+
+module.exports = movieRouter;
